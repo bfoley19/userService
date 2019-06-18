05ab448dbc472a603ccf8c444bf65050db788014
Brent Foley <brent.foley@gmail.com>
2019-06-17 22:48:07 -0600

    Adding unit test project

---
+++ userServiceTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace userServiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
